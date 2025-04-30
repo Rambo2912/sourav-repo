3ba988ad745b2862009d0dfdbc59a14786db406b
rambo291292 <jhonrambo2912@gmail.com>
2025-04-30 13:52:14 +0530

    code for interest rate

---
+++ new.py
@@ -0,0 +1,6 @@
+#programme to calculate interest rate
+p=float(input("enter the principal amout: "))
+r=float(input("enter the interest rate: "))
+t=int(input("enter time : "))
+si=(p*r*t)/100
+print("the simple interest is :" ,si)
